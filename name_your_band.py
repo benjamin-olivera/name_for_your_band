71760c15aac894e02082c8ea03f5fdc50d3cfb59
BRIAN BENJAMIN OLIVERA COLONIO <benjamin.oliveracolonio@gmail.com>
2024-04-18 18:30:24 -0500

    first commit

---
+++ name_your_band.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("Good evening I can help you with your rock band name")
+#2. Ask the user for the city that they grew up in.
+city = input("What city did you grow up in?\n")
+#3. Ask the user for the name of a pet.
+pet= input("What is the name of your favorite pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your rock band name could be: " + city+ " " + pet)
